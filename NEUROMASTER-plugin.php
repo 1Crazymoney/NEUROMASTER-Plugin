d0992ea6705b2aebbaed686a271d8d1c6b21db59
NEUROMASTER <77465345+nxrtoken@users.noreply.github.com>
2021-03-02 23:17:43 +1100

    Update NEUROMASTER-plugin.php

--- NEUROMASTER-plugin.php
+++ NEUROMASTER-plugin.php
@@ -6,9 +6,10 @@
 Plugin Name: NEUROMASTER
 Plugin URI: http://neuromaster.io/plugin
 Description : NEUROMASTER Token Plugin for Wordpress
-
-
-
+Version 1.0.0
+Author: NEUROMASTER
+Licence GPLv2 or later
+Text Domain: NEUROMASTER-plugin
 
 */
 
